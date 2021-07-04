b85811013a39c31dec5decc05ae89e76887f3b2d
Alberto Ammar <albertoammarw@gmail.com>
2021-07-04 10:06:04 -0300

    fix: Exception use self in functions

--- src/CRUD/Exception.php
+++ src/CRUD/Exception.php
@@ -38,25 +38,25 @@ class Exception extends \Exception
     public static function getString($value, $body = null): string
     {
         switch ($value) {
-            case Exception::SERVER_ERROR:
+            case static::SERVER_ERROR:
                 return 'Houve um erro ao executar a ação, favor entrar em contato conosco';
                 break;
 
-            case Exception::UNAUTHORIZED:
-            case Exception::TOKEN_INVALID:
+            case static::UNAUTHORIZED:
+            case static::TOKEN_INVALID:
                 return 'Não Autorizado';
                 break;
 
-            case Exception::DATA_ERROR:
+            case static::DATA_ERROR:
                 // Primeiro Erro
                 return array_values($body)[0][0];
                 break;
 
-            case Exception::DATA_ERROR_GENERAL:
+            case static::DATA_ERROR_GENERAL:
                 return 'Dados da requisição incorretos';
                 break;
 
-            case Exception::NOT_FOUND_RESOURCE:
+            case static::NOT_FOUND_RESOURCE:
                 return 'Recurso não encontrado';
                 break;
 
@@ -68,28 +68,28 @@ class Exception extends \Exception
 
     /**
      * @param string $type
-     * @throws Exception
+     * @throws static
      */
     public static function NotFound($type = '')
     {
-        Exception::exception(Exception::NOT_FOUND_RESOURCE);
+        static::exception(static::NOT_FOUND_RESOURCE);
     }
 
     /**
-     * @throws Exception
+     * @throws static
      */
     public static function Unauthorized()
     {
-        Exception::exception(Exception::UNAUTHORIZED);
+        static::exception(static::UNAUTHORIZED);
     }
 
     /**
      * @param int $errorCode
      * @param null $body
-     * @throws Exception
+     * @throws static
      */
     public static function Exception(int $errorCode, $body = null)
     {
-        throw new Exception(Exception::getString($errorCode, $body), $errorCode, null, $body, );
+        throw new static(static::getString($errorCode, $body), $errorCode, null, $body, );
     }
 }
